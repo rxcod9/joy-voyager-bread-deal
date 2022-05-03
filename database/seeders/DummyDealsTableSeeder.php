6d776d29520dcd2595dffe7d8bc50baca4370adc
Ramakant <gangwar.ramakant@gmail.com>
2022-05-03 19:24:06 +0530

    💚_Seeder-count

--- database/seeders/DummyDealsTableSeeder.php
+++ database/seeders/DummyDealsTableSeeder.php
@@ -14,7 +14,7 @@ class DummyDealsTableSeeder extends Seeder
      */
     public function run()
     {
-        $count = 100;
+        $count = 20;
         Deal::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
